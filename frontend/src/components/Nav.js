895d6538d13e919bead79e902cc1a40688f4e5df
dmeekins <drew.a.meekins@gmail.com>
2022-09-20 19:38:09 -0400

    adding in nav component and branch
    Signed-off-by: dmeekins <drew.a.meekins@gmail.com>

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Nav extends Component {
+    render() { 
+        return (
+            <h1>Nav Page</h1>
+        );
+    }
+}
+ 
+export default Nav;
