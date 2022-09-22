9500dfe5c8b86e82245b38586efc929b354ff924
drewmeekins <drew.a.meekins@gmail.com>
2022-09-22 14:25:55 -0400

    fixing app one more time
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

---
+++ frontend/src/components/RestaurantCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RestaurantCard extends Component {
+    render() { 
+        return (
+            <h1>Restaurant Card Component!</h1>
+        );
+    }
+}
+ 
+export default RestaurantCard;
