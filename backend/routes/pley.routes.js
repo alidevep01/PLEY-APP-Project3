474454a958573e467582ac6e872440ecf66445d1
drewmeekins <drew.a.meekins@gmail.com>
2022-09-24 16:16:12 -0400

    adding restaurant card and fixed postman whitelist issue
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

--- backend/routes/pley.routes.js
+++ backend/routes/pley.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const ctrls = require("../controllers");
 
+
+// we would need to create individual cards on index page for restaurants and then have reviews nested in the restaurants' :id page
 router.get("/", ctrls.pley.index);
 router.post("/", ctrls.pley.create);
 router.delete("/:id", ctrls.pley.destroy);
---
+++ frontend/src/components/RestaurantCard.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+
+let baseURL = ''
+
+class RestaurantCard extends Component {
+    constructor(props){
+      super(props)
+      this.state = {
+        baseURL: "https://api.yelp.com/v3/",
+        apikey: `apikey${process.env.REACT_APP_API_KEY}`,
+        restaurant: '',
+        searchURL: ''
+      }
+    }
+  
+    handleChange = (event) => {
+      event.preventDefault()
+      console.log(event.target.id)
+      console.log(event.target.value)
+  
+      this.setState({
+        [event.target.id]: event.target.value
+      })
+    }
+  
+    handleSubmit = (event) => {
+      event.preventDefault()
+  
+      this.setState({
+        searchURL: this.state.baseURL + this.state.apikey //i have to figure out the rest of this url out.
+      }, () => {
+        fetch(this.state.searchURL)
+          .then((response) => response.json())
+          .then(json => this.setState({
+            restaurant: json,
+            name: ''
+          }), (err) => console.log(err))
+      })
+    }
+  
+    componenetDidMount() {
+      this.getRestaurants()
+    }
+    
+    getRestaurants = (location) => {
+      fetch(baseURL + '/businesses/search?categories=restaurants' + `&location=${location}`, {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer r2iztoK6oWTGflkpSyaMDeTeTgJeib_xGZpXmc9XCIcIzvmdEbzjtYVvQErYKNS89H6WJ_rbsHgQ7QzZQiZJGHbJE5VFh88r5Z44v1CimVobpiteTXRAYBcNQp4rY3Yx'
+        },
+      })
+      .then((res, error) => {
+        if(error){
+          return res.json
+        }else{
+          return []
+        }
+      })
+      .then((data) => {
+        console.log('data', data)
+        this.setState({restaurants: data.restaurants})
+      })
+    }
+  
+    render() { 
+      return (
+        <h1>App Page!</h1>
+      );
+    }
+  }
+ 
+export default RestaurantCard;
