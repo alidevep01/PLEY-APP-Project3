57ba0a462923fd6b31d29a904560af5dba7a69c0
mohdali0326 <mohammadali0326@gmail.com>
2022-09-24 10:43:27 -0500

    created get and post route

--- backend/controllers/pley.ctrls.js
+++ backend/controllers/pley.ctrls.js
@@ -1,7 +1,23 @@
+const db = require("../models");
+
 const index = (req, res) => {
-  res.send("Get route is working");
+  db.Review.find({}, (error, reviews) => {
+    if (error) return res.status(404).json({ error: error.message });
+    return res.status(200).json({
+      reviews,
+      requestedAt: new Date().toLocaleString(),
+    });
+  });
+};
+
+const create = (req, res) => {
+  db.Review.create(req.body, (error, createReview) => {
+    if (error) return res.status(404).json({ error: error.message });
+    return res.status(200).json(createReview);
+  });
 };
 
 module.exports = {
   index,
+  create,
 };
