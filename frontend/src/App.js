0ddf316bca6bdd97686d23a7d875484acf732762
drewmeekins <drew.a.meekins@gmail.com>
2022-10-01 18:04:46 -0400

    search functionality now consoliated into the navbar component and works when searching
    Signed-off-by: drewmeekins <drew.a.meekins@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,55 +29,8 @@ class App extends Component {
   constructor(props){
       super(props)
       this.state = {
-          baseURL: `https://api.spoonacular.com/recipes/complexSearch`,
-          apiKey: `?apiKey=a5bc09310d4c4673a21e07e15aa11be6&`,
-          recipe:''
       }
   }
-
-  handleChange = (event) => {
-      event.preventDefault()
-      console.log(event.target.id)
-      console.log(event.target.value)
-  
-      this.setState({
-          [event.target.id]: event.target.value
-      })
-  }
-
-  handleSubmit = (event) => {
-      event.preventDefault()
-      this.setState({
-          searchURL: this.state.baseURL + this.state.apiKey + 'cuisine=' + this.state.recipe
-      }, () => {
-          fetch(this.state.searchURL)
-              .then((response) => response.json())
-              .then(json => this.setState({
-                  recipe: json,
-                  // giphy: ''
-              }), (err) => console.log(err))
-      })
-      console.log(this.state.recipe)
-  }
-
-  componentDidMount() {
-    this.getReviews();
-  }
-
-  getReviews = () => {
-    fetch(baseURL + "/pley")
-      .then((res) => {
-        if (res.status === 200) {
-          return res.json();
-        } else {
-          return [];
-        }
-      })
-      .then((data) => {
-        console.log(data);
-        this.setState({ reviews: data.reviews });
-      });
-  };
   
   addReview = (review) => {
     const copyReview = [...this.state.reviews]
@@ -91,9 +44,7 @@ class App extends Component {
   }
 
   render() {
-    return (
-
-      
+    return (      
       <div>
         <NavbarPley />
         <br></br>
@@ -101,22 +52,6 @@ class App extends Component {
         <br></br>
         <br></br>
         <h1>Recipe</h1>
-          <form onSubmit={this.handleSubmit}>
-              <label>Title</label>
-              <input
-                  id='recipe'
-                  type='text'
-                  placeholder='Enter Recipe Here'
-                  value={this.state.recipe}
-                  onChange={this.handleChange}
-              />
-
-              <input
-                  type='submit'
-                  value='Search For Recipe'
-              />
-          </form>
-        {/* <RestaurantCard recipes={this.state.recipe}/> */}
         <ReviewForm addReview={this.addReview}/>
         <Footer />
       </div>
