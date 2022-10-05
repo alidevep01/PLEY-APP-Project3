f4046b49f753225678e79d3f1095a3d987c0dbb0
mohdali0326 <mohammadali0326@gmail.com>
2022-10-05 11:26:38 -0500

    port 3001

--- frontend/src/components/RestaurantCard.js
+++ frontend/src/components/RestaurantCard.js
@@ -1,36 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class RestaurantCard extends Component {
-    render() { 
-        if(this.props.recipes.results == null) {
-            <h1>null</h1>
-        }else{
-        return (
-            <div>
-                <h1>Restaurant Card</h1>
-                {/* <img src={this.props.recipes.results}/> */}
-                {this.props.recipes.results.map((results) => {
-                    console.log(results.id)
-                    console.log(results)
-                    
-                    return (
-                    <div>
-                      <a href={results.id}><img src={results.image} alt='image'/></a>
-                      <a href={results.id}><h2>{results.title}</h2></a>
-                    </div>
-                    )
-                    
-                })}
-                {console.log(this.props.recipes.results[0].id)}
-                {/* {this.props.recipes.results.image.map( (data) => {
+  render() {
+    if (this.props.recipes.results == null) {
+      console.log("null");
+    } else {
+      return (
+        <div>
+          <h1>Restaurant Card</h1>
+          {/* <img src={this.props.recipes.results}/> */}
+          {this.props.recipes.results.map((results) => {
+            console.log(results.id);
+            console.log(results);
+
+            return (
+              <div>
+                <a href={results.id}>
+                  <img src={results.image} alt="image" />
+                </a>
+                <a href={results.id}>
+                  <h2>{results.title}</h2>
+                </a>
+              </div>
+            );
+          })}
+          {console.log(this.props.recipes.results[0].id)}
+          {/* {this.props.recipes.results.image.map( (data) => {
                     console.log(results)
                     return <img src='https://spoonacular.com/recipeImages/715769-312x231.jpg' alt=''/>
                 })} */}
-                
-            </div>
-        );
+        </div>
+      );
     }
-    }
+  }
 }
- 
+
 export default RestaurantCard;
